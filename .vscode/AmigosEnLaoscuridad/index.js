2f3ddc3ad544c387b7938466ce2382ce86cf6cf8
Roberto Gonzalez Arriaga <roberto.gonzalez.a739@gmail.com>
2026-05-02 07:37:51 -0600

    Inicio del repositorio

---
+++ .vscode/AmigosEnLaoscuridad/index.js
@@ -0,0 +1,11 @@
+/*
+    4 amigos tienen que cruzar un puente a obscuras en menos de 15 min, pero solo tienen una linterna, asi que 
+    una persona tiene que volver
+
+    Tiempo de las personas en cruzar
+    Persona A = 1min
+    Persona B = 2min
+    Persona C = 5min
+    Persona D = 8min
+
+*/
